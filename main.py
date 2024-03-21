919e7a920334fb8a72f0c2293d92f9302a4fc6c7
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-03-21 11:30:48 +0100

    dernière modif

--- main.py
+++ main.py
@@ -6,29 +6,51 @@ class Person :
         
     def choose_name (self) :
         while True : 
-            self.name = input ("donner votre nom :")
+            name = input ("donner votre nom :")
             try : 
-                assert self.name.isalpha ()
+                assert name.isalpha ()
             except AssertionError : 
-                print ("le nom doit contenir seulement des lettres")
+                print ("le nom doit contenir seulement des lettres !")
                 continue 
             else : 
+                self.name = name
                 return self.name
             
     def choose_symbole (self) : 
         while True : 
-            self.symbol = input ("Donnez un symbole :")
+            symbol = input ("Donnez un symbole :")
             try :
-                assert self.symbol.isalpha () and len(self.symbol) == 1 
+                assert symbol.isalpha () and len(symbol) == 1 
             except AssertionError : 
-                print ("le symbole doit contenir une seule lettre")
+                print ("le symbole doit contenir une seule lettre !")
                 continue
             else : 
+                self.symbol = symbol.upper()
                 return self.symbol 
             
 
+class Menu :
+    
+    def choix (self, choice = input ("choisissez une option :")) :
+        try :
+            assert choice in ["1", "2"]
+        except AssertionError : 
+            print ("choisissez entre la première et la deuxième option !")
+            return self.choix()
+        else : 
+            return choice
+     
+    def display_main_menu (self) :
+        menu = """
+            Bienvenue à X - O game
+                1. Start the game 
+                2. Quit the game 
+            Choose one option :"""
+        choice = input (menu)
+        return self.choix (choice)
+        
+        
 
 if "__main__" == __name__ : 
-    joueur = Person ()
-    joueur.choose_name ()
-    joueur.choose_symbole ()
+    menuu = Menu ()
+    menuu.display_main_menu ()
