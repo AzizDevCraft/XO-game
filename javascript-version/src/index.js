571dcc12a78f8d0f611984facf65c84fbabb855c
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-10-02 18:56:39 +0200

    refactoring Menu.js + mise à jour de l'api

--- javascript-version/src/index.js
+++ javascript-version/src/index.js
@@ -7,5 +7,59 @@ import ConsoleIO from "./consoleIO.js"
 
 const prompt = PromptSync ()
 
+class GameController {
+
+    /**
+     * @param {ConsoleIO} io 
+     */
+    constructor (io) {
+        this.io = io
+        this.board = new Board ()
+        this.players = []
+        this.menu = new Menu ()
+    }
+
+    // #startGame () {
+    //     const startMenu = new Menu ()
+    //     const choice = startMenu.displayMainMenu ()
+    //     if (choice === 2) this.#quitGame ()
+    //     else {
+    //         console.log ("player 1 identifiez-vous :")
+    //         const playerA = new Person ()
+    //         this.players.push (this.#oldPlayer (playerA))
+    //         console.log ("player 2 identifiez-vous :")
+    //         const playerB = new Person (this.players[0]._name, this.players[0]._symbol)
+    //         this.players.push (this.#oldPlayer (playerB))
+    //         return this.#playGame ()
+    //     }
+    // }
+
+    /**
+     * input control
+     * @param {string} message 
+     * @param {(string, string) => {valid:boolean, errorMessage?:string}} validFunc 
+     * @param {string} existingValue 
+     * @returns {string}
+     */
+    askUntilValid (message, validFunc, existingValue) {
+        let input = this.io.read(message)
+        while (!validFunc(input, existingValue).valid) {
+            input = this.io.read(validFunc(input, existingValue).errorMessage +" essaye un autre : ")
+        }
+        return input.trim ()
+    }
+
+    startGame () {
+        this.io.write (this.menu.mainMenu)
+        const choix = this.io.read (this.menu.promptMessage)
+        if (choix === 1) {
+            this.io.write ("player 1 identifiez-vous :")
+            const playerA = new Person (this.askUntilValid ("Donner votre nom : ", Person.validName, ""), this.askUntilValid ("choisissez votre symbol : ", Person.validSymbol, "").toUpperCase ())
+            this.players.push (playerA)
+        }
+    }
+}
 
 
+const party = new GameController (new ConsoleIO (prompt))
+party.startGame ()
---
+++ javascript-version/tests/index.test.js
@@ -0,0 +1,13 @@
+// import GameController from "../src/index.js"
+// import FakeIO from "./FakeIO"
+
+// describe.skip ("tester le debut du jeux", () => {
+    
+//     test ("Happy Path", () => {
+//         const game = new GameController (new FakeIO ([1, "aziz", "X"]))
+//         game.startGame ()
+//         const player = game.players[0]
+//         expect (player._name).toMatch("Aziz")
+//         expect (player._symbol).toMatch("X")
+//     })
+// })
