14ba04310d95387dc75ffe6791251b8e162195b1
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-10-02 17:28:02 +0200

    j'ai ajouté le module ConsoleIO pour me facilité après le teste et j'ai importer les modules dans index.js

---
+++ javascript-version/src/consoleIO.js
@@ -0,0 +1,13 @@
+export default class ConsoleIO {
+    constructor (prompt) {
+        this.prompt = prompt 
+    }
+
+    read (message) {
+        return this.prompt (message)
+    }
+
+    write (message) {
+        console.log (message)
+    }
+}
--- javascript-version/src/index.js
+++ javascript-version/src/index.js
@@ -1,13 +1,11 @@
-// import PromptSync from "prompt-sync"
-const prompt = require ("prompt-sync") ()
-// const Person = require ("./Person")
-// const Menu = require ("./Menu")
-// const Board = require ("./Board")
-// const Game = require ("./Game")
-// import Person from "../src/Person.js"
-// import Menu from "../src/Menu.js"
-// import Board from "../src/Board.js"
-// import Game from "../src/Game.js"
+import PromptSync from "prompt-sync"
+import Person from "../src/Person.js"
+import Menu from "../src/Menu.js"
+import Board from "../src/Board.js"
+import Game from "../src/Game.js"
+import ConsoleIO from "./consoleIO.js"
 
-let IO = prompt("aziz :")
-console.log (IO)
+const prompt = PromptSync ()
+
+
+
