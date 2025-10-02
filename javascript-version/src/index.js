536e08b6648951b35ffa80cf012c944cbccef677
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-10-02 15:52:01 +0200

    nouvelle branche pour création de la partie I/O

---
+++ javascript-version/src/index.js
@@ -0,0 +1,13 @@
+// import PromptSync from "prompt-sync"
+const prompt = require ("prompt-sync") ()
+// const Person = require ("./Person")
+// const Menu = require ("./Menu")
+// const Board = require ("./Board")
+// const Game = require ("./Game")
+// import Person from "../src/Person.js"
+// import Menu from "../src/Menu.js"
+// import Board from "../src/Board.js"
+// import Game from "../src/Game.js"
+
+let IO = prompt("aziz :")
+console.log (IO)
