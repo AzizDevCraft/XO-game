430d61a8b1c64bdb9c13a0a4cdbae80c1c712221
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-11-05 10:15:58 +0100

    Module BoardView done

---
+++ javascript-version/src/io/web/BoardView.js
@@ -0,0 +1,64 @@
+export default class BoardView {
+
+    #board;
+
+    /**
+     * @param {Array<string|number>}
+     */
+    constructor (board, root = document.body) {
+        this.#board = board
+        this.root = root
+    }
+
+    renderBoard () {
+        const boardTemplate = document.querySelector ("#tpl-board")
+        const tplFragment = boardTemplate.content.cloneNode (true)
+        this.root.append (tplFragment)
+        
+        this.cells = this.root.querySelector ("#board").children
+    }
+
+    /**
+     * @param {string} symbol 
+     * @param {number} position 
+     */
+    updateCell (symbol, position) {
+        const playedCell = [].find.call (this.cells, (cell) => Number(cell.dataset.position) === position)
+        if (!playedCell.dataset.play) {
+            const icon = document.createElement("i")
+            icon.setAttribute ("class", `text-7xl fa-solid fa-${symbol.toLowerCase ()}`)
+            playedCell.dataset.play = symbol
+            playedCell.append (icon)
+        }
+        else 
+            return
+    }
+
+    resetBoard () {
+        for (let cell of this.cells) {
+            cell.dataset.play = ""
+            cell.firstElementChild?.remove ()
+        }
+    }
+
+    /**
+     * @param {Array<number>} cells 
+     */
+    highlightWinner (cells) {
+        [].forEach.call (this.cells, (cell) => {
+            if (cells.includes(Number(cell.dataset.position))){
+                cell.firstElementChild.classList.add ("text-red-500")
+            }
+        })
+    }
+
+    /**
+     * @param {Function} handler 
+     */
+    onClick (handler) {
+        [].forEach.call (this.cells, (cell) => {
+            cell.addEventListener ("click", (event) => handler (event, Number (cell.dataset.position)))
+        })
+    }
+    
+}
